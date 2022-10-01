1a580f61eee6e8d899c6bd55eb536e9f6a00a021
Krishna M <krishna.mahadevaiah@gmail.com>
2022-10-01 19:00:00 +0530

    learnt value call

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
+const bdate = document.querySelector("#birthday-date");
+
+const lnum = document.querySelector("#lucky-num");
+
+const find = document.querySelector("#find-out")
+
+
+
+
+function findout()
+{
+    console.log("Hey");
+    // console.log(bdate)
+    console.log(bdate.value) //working
+    const dateofbirth = bdate.value;
+    console.log(dateofbirth) //not working
+    const sumofbdate = countbirthdate(dateofbirth);
+    console.log("sum",sumofbdate)
+   
+}
+function countbirthdate(dateofbirth)
+{
+    dateofbirth = dateofbirth.replaceAll("-","");
+    let sum = 0;
+    for(i=0;i<dateofbirth.length;i++)
+    {
+        sum=sum+Number(dateofbirth.charAt(i));
+
+    }
+    return sum;
+
+    
+}
+
+
+find.addEventListener("click",findout);
+
+
+
+
 
